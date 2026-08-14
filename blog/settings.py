a4fa81245a3eb8cd5a3e70b04c68e5265e83b42a
Palak <palakbatra@42works.net>
2026-08-14 13:37:18 +0530

    Configure Railway MYSQL

--- blog/settings.py
+++ blog/settings.py
@@ -108,11 +108,11 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("railway"),
-        "USER": os.getenv("root"),
-        "PASSWORD": os.getenv("hJGTnZsZCYuSkRZDDkiHOTLOHsfGBpUx"),
-        "HOST": os.getenv("mysql-7l15.railway.internal"),
-        "PORT": os.getenv("3306"),
+        "NAME": os.getenv("MYSQLDATABASE"),
+        "USER": os.getenv("MYSQLUSER"),
+        "PASSWORD": os.getenv("MYSQLPASSWORD"),
+        "HOST": os.getenv("MYSQLHOST"),
+        "PORT": os.getenv("MYSQLPORT"),
     }
 }
 
