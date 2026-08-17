cb5a15558f0a391177f11804bcd6a9a6fac8bfaf
Palak <palakbatra@42works.net>
2026-08-17 13:27:54 +0530

    Fix setting final

--- blog/settings.py
+++ blog/settings.py
@@ -38,17 +38,15 @@ DEBUG = os.getenv("DEBUG") == "True"
 #     "registration-form-production-8941.up.railway.app"
 # ]
 
-ALLOWED_HOSTS = [
-    "registration-form-production-8941.up.railway.app",
-    "your-custom-domain.com",
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = (
+    os.getenv("ALLOWED_HOSTS", "").split(",")
+    if os.getenv("ALLOWED_HOSTS")
+    else []
+)
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://registration-form-production-8941.up.railway.app",
-    "https://your-custom-domain.com",
-]
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") if os.getenv("ALLOWED_HOSTS") else []
+
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",") if os.getenv("CSRF_TRUSTED_ORIGINS") else []
 # Application definition
 
 INSTALLED_APPS = [
