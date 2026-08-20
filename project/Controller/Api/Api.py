5dc16a8cc8a06056a3092964c8a6efeb0c0ca4df
Palak <palakbatra@42works.net>
2026-08-20 11:38:02 +0530

    update readme ,requirement file

--- project/Controller/Api/Api.py
+++ project/Controller/Api/Api.py
@@ -31,8 +31,7 @@ def Add_list(request):
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    print(serializer.errors)
+        print(serializer.errors)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
